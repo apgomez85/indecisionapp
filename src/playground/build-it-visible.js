2efd68dd212d2090f2a7e653b73f0909fc3f7718
Adrian <apgomez85@gmail.com>
2018-12-03 08:02:31 -0800

    setState

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,30 +1,67 @@
-const app = {
-  title: 'Visibility Toggle',
-  visibleDetails: true
-};
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Visibility Toggle',
+      visibility: false
+    };
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+  }
 
-const appRoot = document.getElementById('app');
+  handleToggleVisibility() {
+    this.setState(() => {
+      return {
+        visibility: !this.state.visibility
+      };
+    });
+  }
 
-const onToggleDetails = () => {
-  app.visibleDetails = !app.visibleDetails;
-  render();
-};
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility === true ? 'Hide Details' : 'Show Details'}
+        </button>
+        <p>
+          {this.state.visibility === true
+            ? 'Hey, these are some details you can now see.'
+            : ''}
+        </p>
+      </div>
+    );
+  }
+}
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={onToggleDetails}>
-        {app.visibleDetails === true ? 'Hide Details' : 'Show Details'}
-      </button>
-      <p>
-        {app.visibleDetails === true
-          ? 'Hey, these are some details you can now see.'
-          : ''}
-      </p>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-render();
+// const app = {
+//   title: 'Visibility Toggle',
+//   visibleDetails: true
+// };
+
+// const appRoot = document.getElementById('app');
+
+// const onToggleDetails = () => {
+//   app.visibleDetails = !app.visibleDetails;
+//   render();
+// };
+
+// const render = () => {
+//   const template = (
+//     <div>
+//       <h1>{app.title}</h1>
+//       <button onClick={onToggleDetails}>
+//         {app.visibleDetails === true ? 'Hide Details' : 'Show Details'}
+//       </button>
+//       <p>
+//         {app.visibleDetails === true
+//           ? 'Hey, these are some details you can now see.'
+//           : ''}
+//       </p>
+//     </div>
+//   );
+//   ReactDOM.render(template, appRoot);
+// };
+
+// render();
