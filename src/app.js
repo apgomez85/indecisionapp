790f23271f59d09b3ea4534570ba7e9149ea288d
Adrian <apgomez85@gmail.com>
2019-01-07 11:56:05 -0800

    webpack with babel

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-import { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-// import { square, add } from './utils.js';
-
-// console.log('app.js is running');
-
-// console.log(square(4));
-// console.log(add(100, 23));
-
-console.log(isAdult(18));
-
-console.log(canDrink(18));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
