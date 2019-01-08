beb97b3f754d5251077b9874ae9ee5942d8f885b
Adrian <apgomez85@gmail.com>
2019-01-08 07:40:57 -0800

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
@@ -14,5 +15,9 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
   }
 };
